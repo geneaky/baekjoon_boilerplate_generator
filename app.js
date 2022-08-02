298f5f3700747027b301388ad2330444cd7ed085
geneaky <kaktus418@gmail.com>
2022-08-02 15:34:04 +0900

    back up

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 const chalk = require('chalk');
 const Progress = require('progress');
 const inquirer = require('inquirer');
-
 const commander = new Command();
 const bar = new Progress('running [:bar] :percent :etas', {
     complete: '=',
@@ -14,27 +13,18 @@ const bar = new Progress('running [:bar] :percent :etas', {
 const configFile = fs.readFileSync("./config.json", 'utf8');
 const configData = JSON.parse(configFile);
 
-const makeProblemAfterSetting = (project_path, package_name, problem_id) => {
-    console.log('hi');
-}
-const makeProblemClass = (problem_id) => {
-    console.log('hi');
-}
-
-if(!configData.project_path && !configData.package_name) {
+if(!configData.project_path || !configData.package_name || !configData.language_type) {
     commander.action((cmd,args) => {
             inquirer.prompt([
                 {
                     type: 'input',
                     name: 'project_path',
                     message: '프로젝트 경로를 입력하세요',
-                    default: '.',
                 },
                 {
                     type: 'input',
                     name: 'package_name',
                     message: '패키지명을 입력하세요',
-                    default: '.',
                 },
                 {
                     type: 'list',
@@ -49,8 +39,12 @@ if(!configData.project_path && !configData.package_name) {
                     default: 'XXX',
                 }
             ]).then((answer) => {
+                if(!answer.project_path || !answer.package_name || !answer.language_type || !answer.problem_id) {
+                    console.log(chalk.red('Command Not Found'));
+                    return;
+                }
                 bar.tick();
-                makeProblemAfterSetting(answer.project_path,answer.package_name,answer.language_type, answer.problem_id);
+                makeProblemAfterSetting(answer.project_path, answer.package_name, answer.language_type, answer.problem_id);
                 if(bar.complete) {
                     console.log(chalk.yellow('\ncomplete\n'));
                 }
@@ -69,8 +63,12 @@ if(!configData.project_path && !configData.package_name) {
                 default: 'XXX',
             }
         ]).then((answer) => {
+            if(!answer.problem_id) {
+                console.log(chalk.red('Command Not Found'));
+                return;
+            }
             bar.tick()
-            makeProblemClass(problem_id);
+            makeProblemClass(answer.problem_id);
             if(bar.complete) {
                 console.log(chalk.yellow('\ncomplete\n'));
             }
@@ -78,10 +76,49 @@ if(!configData.project_path && !configData.package_name) {
     })
         .parse(process.argv);
 }
-//세팅 단계 - 프로젝트 위치 (현재위치로 하시겠습니까?) , 패키지명
-    //세팅 수정 옵션을 따로 주고 추후 수정 가능
-    //세팅이 되어 있으면 이 질문은 다시 물어보지 않는다.
-    //패키지 위치 입력 src/main/java/{project_name}//
 
-//백준 번호 입력
-//타입 선택 1. java , 2. kotlin,
+const makeProblemAfterSetting = (project_path, package_name, language_type, problem_id) => {
+    configData.project_path = project_path;
+    configData.package_name = package_name;
+    configData.language_type = language_type;
+    fs.writeFileSync("./config.json", JSON.stringify(configData));
+
+    if(language_type == "Java") {
+        fs.writeFileSync(project_path+"\\"+package_name+"\\bj"+problem_id+"\\BJ"+problem_id+".java", getJavaTemplate(project_path,package_name,problem_id));
+        fs.writeFileSync(project_path+"\\"+package_name+"\\bj"+problem_id+"\\input.txt", "");
+    }else if (language_type == "Kotlin") {
+        fs.writeFileSync(project_path+"\\"+package_name+"\\bj"+problem_id+"\\BJ"+problem_id+".kt", getKotlinTemplate(project_path,package_name,problem_id));
+        fs.writeFileSync(project_path+"\\"+package_name+"\\bj"+problem_id+"\\input.txt", "");
+    }
+}
+const makeProblemClass = (problem_id) => {
+    if(configData.language_type == "Java") {
+        fs.writeFileSync(configData.project_path+"\\"+configData.package_name+"\\bj"+problem_id+"\\BJ"+problem_id+".java", getJavaTemplate(configData.project_path,configData.package_name,problem_id));
+        fs.writeFileSync(configData.project_path+"\\"+configData.package_name+"\\bj"+problem_id+"\\input.txt","");
+    }else if (configData.language_type == "Kotlin") {
+        fs.writeFileSync(configData.project_path+"\\"+configData.package_name+"\\bj"+problem_id+"\\BJ"+problem_id+".kt", getKotlinTemplate(configData.project_path,configData.package_name,problem_id));
+        fs.writeFileSync(configData.project_path+"\\"+configData.package_name+"\\bj"+problem_id+"\\input.txt","");
+    }
+}
+
+const getJavaTemplate = (project_path, package_name, problem_id) => {
+    return `package ${package_name+".bj"+problem_id}
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream(${project_path+"/"+package_name+"/b"+problem_id+"/input.txt"});
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+    }
+}`
+}
+
+const getKotlinTemplate = (project_path, package_name, problem_id) => {
+    return "";
+}
